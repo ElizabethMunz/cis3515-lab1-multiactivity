16718cabccc18532374401d98a81f92ecdba3513
ElizabethMunz <munz@temple.edu>
2018-09-12 09:45:04 -0400

    implemented onClickListener for ButtonA

--- app/src/main/java/edu/temple/munz/multiactivity/ActivityA.java
+++ app/src/main/java/edu/temple/munz/multiactivity/ActivityA.java
@@ -1,13 +1,31 @@
 package edu.temple.munz.multiactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityA extends AppCompatActivity {
+
+    Button buttonA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonA = findViewById(R.id.buttonA);
+
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //start activityB
+                Intent startActivityIntent = new Intent(ActivityA.this, ActivityB.class);
+                startActivity(startActivityIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/edu/temple/munz/multiactivity/ActivityB.java
+++ app/src/main/java/edu/temple/munz/multiactivity/ActivityB.java
@@ -1,13 +1,20 @@
 package edu.temple.munz.multiactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityB extends AppCompatActivity {
+
+    Button buttonB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+
     }
+
 }
